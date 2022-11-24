d942c15290f5949390d4b6c745e0d0d551997962
Jawad Ahmed <jawad.w.ahmed@gmail.com>
2022-11-24 18:58:39 +0000

    nifty little pattern

--- lc_interpreter/bin/expr.ml
+++ lc_interpreter/bin/expr.ml
@@ -5,14 +5,14 @@ type 'a spanned = { span : source_span; node : 'a }
 let ( + ) lhs rhs = { start = lhs.start; finish = rhs.finish }
 let map_span f s = { s with node = f s.node }
 
-type expr =
+type expr' =
   | EIntLit of int
-  | ELet of { name : ident; body : spanned_expr }
-  | EFun of { args : ident list; body : spanned_expr }
+  | ELet of { name : ident; body : expr }
+  | EFun of { args : ident list; body : expr }
   | EIdent of ident
-  | EBinOp of { op : bin_op spanned; lhs : spanned_expr }
-  | EApp of { func : spanned_expr; arg : spanned_expr }
+  | EBinOp of { op : bin_op spanned; lhs : expr }
+  | EApp of { func : expr; arg : expr }
 
+and expr = expr' spanned
 and ident = string spanned
-and spanned_expr = expr spanned
 and bin_op = Add | Sub | Mul | Div
