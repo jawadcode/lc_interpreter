e9f6e5057af1088e5180e18df5c78eb64bac2fd2
Jawad Ahmed <jawad.w.ahmed@gmail.com>
2022-11-26 00:24:12 +0000

    b

--- lc_interpreter/bin/main.ml
+++ lc_interpreter/bin/main.ml
@@ -4,6 +4,5 @@ let () =
   let test = "add 123 123" in
   let tokens = lex test in
   Printf.printf "Test string: \"%s\"\nTokens:\n" test;
-
   Seq.map string_of_token tokens
   |> Seq.iter print_endline
