97cd7e1924b79eb1852e1008ca1f5cdbf29cad71
jawadcode <jawad.w.ahmed@gmail.com>
2023-02-25 18:04:41 +0000

    rewording

--- lc_interpreter/bin/main.ml
+++ lc_interpreter/bin/main.ml
@@ -5,7 +5,7 @@ let () =
   let rec f () =
     let test = read_line () in
     let tokens = lex test in
-    Printf.printf "Test string: \"%s\"\nTokens:\n" test;
+    Printf.printf "Source: \"%s\"\nTokens:\n" test;
     Seq.map string_of_token tokens |> Seq.iter print_endline;
     f ()
   in f ()
